71af09dd5f9c4f4f8e53b3daf3b639576e4f6047
kursant <Mateusz Choma>
2018-06-27 14:02:45 +0200

    Works with transitions

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const menuToggle = document.querySelector('.menu__toggle')
 const menuList = document.querySelector('.menu__list')
 
+const menuListHeight = menuList.clientHeight
+menuList.classList.add('menu__list--collapsed')
+
 menuToggle.addEventListener(
     'click',
     (event) => {
@@ -8,6 +11,7 @@ menuToggle.addEventListener(
 
         if(menuList.classList.contains('menu__list--collapsed')){
             menuList.classList.remove('menu__list--collapsed')
+            menuList.style.maxHeight = menuListHeight + 'px'
         }else{
             menuList.classList.add('menu__list--collapsed')
         }
