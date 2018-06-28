fe99cefad6099989afa7d1f6bb70393f4698d372
kursant <Mateusz Choma>
2018-06-28 12:44:41 +0200

    Fixed menu animation

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const menuToggle = document.querySelector('.menu__toggle')
 const menuList = document.querySelector('.menu__list')
+const menuListItems = document.querySelectorAll('.menu__list-item')
 
-const menuListHeight = menuList.clientHeight
+const menuListHeight = menuListItems[0].clientHeight * menuListItems.length
 menuList.classList.add('menu__list--collapsed')
 
 menuToggle.addEventListener(
